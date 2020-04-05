bb297d1fef139e38234ad596e388a4159492dcda
0xFr0zen <0xfr0zen@gmail.com>
2020-04-05 21:04:44 +0200

    Update main.js

--- src/res/main.js
+++ src/res/main.js
@@ -1,8 +1,6 @@
 $(document).ready(() => {
     var ctx = $('canvas')[0].getContext('2d');
     var draw = function (video, dt) {
-        console.log(video);
-
         ctx.drawImage(video, 0, 0);
     };
     var myCamvas = new camvas(ctx, draw);
