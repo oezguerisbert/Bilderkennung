606903c164eeb7d1371503e3f138476b4e9a99a5
0xFr0zen <0xfr0zen@gmail.com>
2020-04-28 15:25:19 +0200

    updates

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import * as Jimp from 'jimp';
 import Chunk, { ChunkI } from './src/Chunk';
 import Pixel, { Color } from './src/Pixel';
+
+// Bild lesen
 Jimp.read('input/img.jpg', (err: Error, image) => {
     if (err) throw err;
+    //Bildobjekt-Kopie
     let image2 = image;
+
+    //Bilddaten (groeßen)
     let width = image2.getWidth();
     let height = image2.getHeight();
+
+    //Chunkoptionen [einzelne Teilbilder - Eigenschaften]
     let chunkoptions: ChunkI = {
         width: 0,
         height: 0,
@@ -15,6 +22,8 @@ Jimp.read('input/img.jpg', (err: Error, image) => {
     chunkoptions.width = Math.floor(width / howManyChunksPerRow);
     chunkoptions.height = Math.floor(height / howManyChunksPerRow);
     let squareCh = howManyChunksPerRow * howManyChunksPerRow;
+
+    //initialisierung Chunks (Array)
     let chunks: Array<Jimp> = new Array<Jimp>();
     for (let i = 0; i < howManyChunksPerRow; i++) {
         for (let j = 0; j < howManyChunksPerRow; j++) {
@@ -22,6 +31,7 @@ Jimp.read('input/img.jpg', (err: Error, image) => {
             let y = j * chunkoptions.height;
             // console.log(`${x},${y}`);
             let url = `output/cut/img-x${x}-y${y}.png`;
+            //Beladen der Chunks
             chunks.push(
                 image
                     .cloneQuiet()
@@ -29,6 +39,7 @@ Jimp.read('input/img.jpg', (err: Error, image) => {
             );
         }
     }
+    //Ausschreiben der Chunks ins Dateisystem
     for (let i = 0; i < chunks.length; i++) {
         let img = chunks[i];
         img.write(`output/cut/${chunks.indexOf(img)}.png`);
