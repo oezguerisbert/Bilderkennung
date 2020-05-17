5cd9f053a97afc9d0ed926a8f402ddbb8892830e
0xFr0zen <0xfr0zen@gmail.com>
2020-05-17 23:01:10 +0200

    Changing to OpenCV and Python

--- index.ts
+++
@@ -1,7 +0,0 @@
-import Main from './src/Main';
-
-let main: Main = new Main();
-main.on('ready', (_) => {
-    // console.log('ready');
-    main.start();
-});
---
+++ install.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+export DEBIAN_FRONTEND=noninteractive
+echo "Installing dependencies";
+sudo apt-get install python -y -qq > /dev/null;
+echo "Installing Python Modules";
+pip install numpy matplotlib opencv-python;
+echo "Installed";
