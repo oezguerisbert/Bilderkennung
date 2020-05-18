765b7fc0735d26023ff3aa12238f96fc501d6c04
0xFr0zen <0xfr0zen@gmail.com>
2020-05-18 10:57:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Installing dependencies";
 sudo apt-get install python -y -qq > /dev/null;
 echo "Installing Python Modules";
-pip install numpy matplotlib opencv-python;
+pip install numpy matplotlib opencv-python ColorIt;
 echo "Installed";
