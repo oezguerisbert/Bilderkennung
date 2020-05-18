efd6a9831da571e4173468d07f172b215665485b
0xFr0zen <0xfr0zen@gmail.com>
2020-05-18 12:32:30 +0200

    Update install.sh
    Fixed Resource issues on Raspberry.

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!bin/bash
-export DEBIAN_FRONTEND=noninteractive
+#export DEBIAN_FRONTEND=noninteractive
 echo "Installing dependencies";
-sudo apt-get install python -y -qq > /dev/null;
+sudo apt-get install libatlas-base-dev libjasper-dev libqtgui4 python3-pyqt5 libqt4-test python3 -y -qq;
+ 
 echo "Installing Python Modules";
-pip install numpy matplotlib opencv-python ColorIt;
+pip3 install numpy matplotlib opencv-python ColorIt;
 echo "Installed";
