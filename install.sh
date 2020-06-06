6d21b082b6e8fcfccbd071c7ad0b0cbc3716ee7a
0xFr0zen <0xfr0zen@gmail.com>
2020-06-06 12:37:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo "Installing dependencies";
 sudo apt-get install libatlas-base-dev libjasper-dev libqtgui4 python3-pyqt5 libqt4-test python3 -y -qq;
 sudo apt-get install libhdf5-dev -y -qq;
 sudo apt-get install libhdf5-serial-dev -y -qq;
-sudo apt-get install libcblas-dev-y -qq;
+sudo apt-get install libcblas-dev -y -qq;
 
 echo "Installing Python Modules";
 pip3 install -r ./requirements.txt;
